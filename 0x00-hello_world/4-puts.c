fe12d9a4d1ff2c93f9278beb9d0976b84457cfeb
Sidmoses73 <sidmoses73@gmail.com>
2023-02-16 07:27:00 -0800

    Task 4 Hello put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - I write a C program without using printf but puts function
+ * Return: 0 and the program end
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
