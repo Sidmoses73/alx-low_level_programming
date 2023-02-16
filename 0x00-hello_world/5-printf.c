7bd5ed67ca48ee9d6794b33d7d79ae9b30a6e346
Sidmoses73 <sidmoses73@gmail.com>
2023-02-16 07:55:01 -0800

    Task 5 Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <sdtio.h>
+/**
+ * main - Programe that prints using the function printf
+ * Return: 0
+ * programe must compile without warning using gcc option
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
