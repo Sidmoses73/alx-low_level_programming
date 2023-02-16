9920ef7d2f27ade01dbaa959315d6b00598b7188
Sidmoses73 <sidmoses73@gmail.com>
2023-02-16 10:43:45 -0800

    Task 8 UNIX is basically a simple operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 /**
  * main - A C program that print exactly the giving line
- * Decription: 'You are not allowed to use any functions listed'
+ * Decription: 'You are not allowed to use any functions'
  * Return: 1 (Success)
  */
 int main(void)
