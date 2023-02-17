a463bb312fc1f3199f1d0a7c4eb4996ad76adf3c
Sidmoses73 <sidmoses73@gmail.com>
2023-02-17 08:21:11 -0800

    Task 2 I sometime suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry Line alphabet in lower case
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
