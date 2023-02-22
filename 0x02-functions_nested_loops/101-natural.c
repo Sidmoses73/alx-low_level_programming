189ba06f1980ffa17725d0cb74434ddb11c0b08f
Sidmoses73 <sidmoses73@gmail.com>
2023-02-21 18:20:51 -0800

    Task 13 Nature made the natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,16 +10,14 @@
 int main(void)
 
 {
-	int i, z = 0;
+	int i, sum = 0;
 
-	while (i < 1024)
+	for (i = 0; i < 1024; i++)
 	{
-		if ((i % 3 == 0) || (i % 5 == 0))
-		{
-			z += i;
-		}
-		i++;
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
 	}
-	printf("%d\n". z);
+	printf("%d\n", sum);
+
 	return (0);
 }
