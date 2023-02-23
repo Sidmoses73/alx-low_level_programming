d34bafa68b6e98f5f34cbc6ee0eb9a65b16dad05
Sidmoses73 <sidmoses73@gmail.com>
2023-02-23 03:33:39 -0800

    Task 2 Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - This will checks for a digit (0 through 9).
+ * @a: a - Variable
+ * @b: b - variable
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	int res;
+
+	int res = a * b;
+
+	return (res);
+}
