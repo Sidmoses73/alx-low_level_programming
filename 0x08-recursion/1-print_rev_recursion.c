f3599239b1da6f871671d7f17af23c8c9bf3cc46
Sidmoses73 <sidmoses73@gmail.com>
2023-03-08 02:34:01 -0800

    Task 1 Why is it so important to dream

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Print in reverse
+ * @s: The input
+ *
+ * Return: 0 Success
+ */
+
+void _print_rev_recursion(char *s)
+
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
