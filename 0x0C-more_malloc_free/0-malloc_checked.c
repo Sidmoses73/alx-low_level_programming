3162f936d1857656c0c53dc6885bb43051cf7057
Sidmoses73 <sidmoses73@gmail.com>
2023-03-16 03:49:19 -0700

    Task 0 Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - Allocate memory using this
+ * Number of byte allocated
+ *
+ * Return: Pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,2 @@
+# 0x0C. C - More malloc, free
+## Its all about more memory allocation
