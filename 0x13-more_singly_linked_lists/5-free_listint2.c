8b432881b30451f163721428b2dd38894f4eff90
Sidmoses73 <sidmoses73@gmail.com>
2023-04-03 02:34:32 -0700

    Task 5 Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: pointer to pointer to the listint_t list to be freed
+ */
+void free_listint2(listint_t **head)
+
+{
+	listint_t *ptr;
+
+	if (head == NULL)
+	return;
+
+	while (*head)
+	{
+		ptr = (*head)->next;
+		free(*head);
+		*head = ptr;
+	}
+
+	*head = NULL;
+}
